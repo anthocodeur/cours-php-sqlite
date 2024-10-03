2aae40a8410a1a93003087ff1ddeb1b9f3c66ec8
anthocodeur <antho.codeur@gmail.com>
2024-10-03 08:55:45 +0200

    add image with custom wf

--- index.php
+++ index.php
@@ -1,6 +1,3 @@
 <?php
 
 echo "Bonjour, monde!";
-$db = new SQLite3('test.db');
-$db->exec('CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY, name TEXT)');
-echo "Base de données créée avec succès!";
